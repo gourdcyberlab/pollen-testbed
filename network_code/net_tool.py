41485823a61d7e5667137413365d26fc19376b49
tboz203 <tboz203@gmail.com>
2014-10-19 20:33:42 -0500

    net_tool.py working correctly now

--- network_code/net_tool.py
+++ network_code/net_tool.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 from __future__ import print_function
 from optparse import OptionParser
+import os
 import sys
 import socket
 import select
@@ -8,8 +9,12 @@ import select
 # found this on the internet. really wishing it came with comments. works
 # rather much like netcat does, so we're on the right track.
 
+# this was written by a C guy.
+# buffers correctly now, and uses select correctly.
+
 class NetTool:
     def run(self):
+        self.stdin = os.fdopen(sys.stdin.fileno(), 'r', 1)
         self.parse_options()
         self.connect_socket()
         self.forward_data()
@@ -68,15 +73,11 @@ class NetTool:
         if(self.connect):
             # initiate connection to remote host
             self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.socket.connect( (self.hostname, self.port) )
+            self.socket.connect((self.hostname, self.port))
         else:
             # listen and wait for a connection
             server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,  1)
-            # try:
-            #     server.setsockopt(socket.SOL_SOCKET, socket.TCP_NODELAY, 1)
-            # except socket.error:
-            #     sys.stderr.write("[-] Warning: unable to set TCP_NODELAY...\n")
+            server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             server.bind(('0.0.0.0', self.port))
             server.listen(1)
             self.socket, address = server.accept()
@@ -84,28 +85,44 @@ class NetTool:
 
     def forward_data(self): # {{{
         self.socket.setblocking(0)
-        while(1):
-            read_ready, write_ready, in_error = select.select([self.socket, sys.stdin], [], [self.socket, sys.stdin])
-            try:
-                buf = self.socket.recv(100)
-                while(buf != ''):
-                    sys.stdout.write(buffer)
-                    sys.stdout.flush()
-                    buf = self.socket.recv(100)
+        while True:
+            # {{{
+            # read_ready, write_ready, in_error = select.select([self.socket, sys.stdin], [], [self.socket, sys.stdin])
+            # try:
+            #     buf = self.socket.recv(100)
+            #     while(buf != ''):
+            #         sys.stdout.write(buffer)
+            #         sys.stdout.flush()
+            #         buf = self.socket.recv(100)
+            #     if(buf == ''):
+            #         return
+            # except socket.error as e:
+            #     sys.stderr.write('[-] Error: {}\n'.format(e))
+            #     pass
+            # while(True):
+            #     r, w, e = select.select([sys.stdin],[],[],0)
+            #     if(len(r) == 0):
+            #         break;
+            #     c = sys.stdin.read(1)
+            #     if(c == ''):
+            #         return
+            #     if(self.socket.sendall(c) != None):
+            #         return
+            # }}}
+
+            read_ready, write_ready, in_error = select.select(
+                    [self.socket, self.stdin], [], [self.socket, self.stdin])
+            if read_ready[0] == self.socket:
+                buf = self.socket.recv(256)
                 if(buf == ''):
                     return
-            except socket.error as e:
-                sys.stderr.write('[-] Error: {}\n'.format(e))
-                pass
-            while(1):
-                r, w, e = select.select([sys.stdin],[],[],0)
-                if(len(r) == 0):
-                    break;
-                c = sys.stdin.read(1)
-                if(c == ''):
-                    return;
-                if(self.socket.sendall(c) != None):
+                sys.stdout.write(buf)
+                sys.stdout.flush()
+            elif read_ready[0] == self.stdin:
+                buf = self.stdin.readline()
+                if(buf == ''):
                     return
+                self.socket.sendall(buf)
         # }}}
 
 if __name__ == '__main__':
