4c07123465171ab5ee5c7002641de2f9cbac9c89
cyberlab <cyberlab@switch.(none)>
2014-10-20 12:21:04 -0500

    removed duplicated directory 'socket_code'

--- network_code/net_tool.py
+++ network_code/net_tool.py
@@ -86,32 +86,8 @@ class NetTool:
     def forward_data(self): # {{{
         self.socket.setblocking(0)
         while True:
-            # {{{
-            # read_ready, write_ready, in_error = select.select([self.socket, sys.stdin], [], [self.socket, sys.stdin])
-            # try:
-            #     buf = self.socket.recv(100)
-            #     while(buf != ''):
-            #         sys.stdout.write(buffer)
-            #         sys.stdout.flush()
-            #         buf = self.socket.recv(100)
-            #     if(buf == ''):
-            #         return
-            # except socket.error as e:
-            #     sys.stderr.write('[-] Error: {}\n'.format(e))
-            #     pass
-            # while(True):
-            #     r, w, e = select.select([sys.stdin],[],[],0)
-            #     if(len(r) == 0):
-            #         break;
-            #     c = sys.stdin.read(1)
-            #     if(c == ''):
-            #         return
-            #     if(self.socket.sendall(c) != None):
-            #         return
-            # }}}
-
             read_ready, write_ready, in_error = select.select(
-                    [self.socket, self.stdin], [], [self.socket, self.stdin])
+                    [self.socket, self.stdin], [], [])
             if read_ready[0] == self.socket:
                 buf = self.socket.recv(256)
                 if(buf == ''):
